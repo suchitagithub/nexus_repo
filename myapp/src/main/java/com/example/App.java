54f3f30af8cabd6b3f06111859be830ff9bc9f30
Ubuntu <ubuntu@ip-172-31-20-229.ec2.internal>
2024-07-26 07:25:18 +0000

    day5

---
+++ myapp/src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+// src/main/java/com/example/App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
